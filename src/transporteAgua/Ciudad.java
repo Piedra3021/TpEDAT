e0babac4c6ba957ee7dba8de6adc4b9c6442a592
Pedro <pbu.101204@gmail.com>
2025-07-05 23:10:09 -0300

    modificado ciudad

--- src/transporteAgua/Ciudad.java
+++ src/transporteAgua/Ciudad.java
@@ -1,5 +1,15 @@
 package transporteAgua;
 
 public class Ciudad {
-    
+    String nombre;
+    String nomeclatura;
+    double metros;
+    int cantHabitantes = 1000;
+    double consumoPromedio = 0.25;
+
+
+    public Ciudad(String nombreC, double metros){
+        this.nombre = nombreC;
+        this.metros = metros;
+    }
 }
