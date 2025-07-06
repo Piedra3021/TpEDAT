ac6b3f00d9acb3a898981f84dc0873a77346d0eb
Your Name <you@example.com>
2025-07-05 23:42:42 -0300

    Anio v0.1

--- src/transporteAgua/Ciudad.java
+++ src/transporteAgua/Ciudad.java
@@ -6,10 +6,37 @@ public class Ciudad {
     double metros;
     int cantHabitantes = 1000;
     double consumoPromedio = 0.25;
+    // Arreglo de anios?
+    Anio consumoProm;
 
-
-    public Ciudad(String nombreC, double metros){
+    public Ciudad(String nombreC, double metros) {
         this.nombre = nombreC;
         this.metros = metros;
+        this.consumoProm = new Anio(2025);
     }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getNomeclatura() {
+        return nomeclatura;
+    }
+
+    public double getMetros() {
+        return metros;
+    }
+
+    public int getCantHabitantes() {
+        return cantHabitantes;
+    }
+
+    public double getConsumoPromedio() {
+        return consumoPromedio;
+    }
+
+    public Anio getConsumoProm() {
+        return consumoProm;
+    }
+
 }
