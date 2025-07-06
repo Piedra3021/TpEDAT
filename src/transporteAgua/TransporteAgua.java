13e8af51496c6cb3e236f47499ea0560fc06acd1
Your Name <you@example.com>
2025-07-05 22:53:10 -0300

    TransporteAgua main

--- src/transporteAgua/TransporteAgua.java
+++ src/transporteAgua/TransporteAgua.java
@@ -1,5 +1,14 @@
 package transporteAgua;
 
+import conjuntistas.ArbolAVL;
+import grafos.Grafo;
+import jerarquicas.*;
+import lineales.*;
+
 public class TransporteAgua {
-    
+    public static void main() {
+
+        ArbolAVL ciudades = new ArbolAVL();
+
+    }
 }
